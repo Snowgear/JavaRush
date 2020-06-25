4e445814b4881e66a438de3fbb43358d0507caf9
Ilya <kirpi495@gmail.com>
2020-06-25 13:52:26 +0300

    Add files via upload

---
+++ Четные и нечетные циферки.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task06.task0606;
+
+/* 
+Чётные и нечётные циферки
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.*;
+
+public class Solution {
+
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+        while (n > 0){
+            if (n % 2 == 0) {
+                even++;
+            }
+            else odd++;
+            n /= 10;
+        }
+        System.out.println("Even: " + even + " Odd: " + odd);
+    }
+}
